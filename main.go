ebeeb7137872e09399198926117dd6c3fc0206bf
Habibmahaasin <habibirf23@gmail.com>
2023-06-12 20:01:38 +0700

    testing change port for deploy

--- main.go
+++ main.go
@@ -37,12 +37,12 @@ func setup() (*gorm.DB, config.Conf, *gin.Engine) {
 }
 
 func main() {
-	conf, err := config.Init()
+	_, err := config.Init()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	router := routesV1.Init(setup()) //Version 1
-
-	router.Run(":" + conf.App.Port)
+	router.Run()
+	// router.Run(":" + conf.App.Port)
 }

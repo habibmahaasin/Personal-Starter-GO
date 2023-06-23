805f1f6e3bb109a8d0a66ed37196584c84f8e264
Habibmahaasin <habibirf23@gmail.com>
2023-06-23 15:54:02 +0700

    update mock using mockgen & notes

--- modules/v1/utilities/device/service/mock/mock_service.go
+++ modules/v1/utilities/device/service/mock/mock_service.go
@@ -1,1 +1,193 @@
-package mock
+package mock_service
+
+import (
+	models "GuppyTech/modules/v1/utilities/device/models"
+	reflect "reflect"
+
+	gomock "github.com/golang/mock/gomock"
+)
+
+// MockService is a mock of Service interface.
+type MockService struct {
+	ctrl     *gomock.Controller
+	recorder *MockServiceMockRecorder
+}
+
+// MockServiceMockRecorder is the mock recorder for MockService.
+type MockServiceMockRecorder struct {
+	mock *MockService
+}
+
+// NewMockService creates a new mock instance.
+func NewMockService(ctrl *gomock.Controller) *MockService {
+	mock := &MockService{ctrl: ctrl}
+	mock.recorder = &MockServiceMockRecorder{mock}
+	return mock
+}
+
+// EXPECT returns an object that allows the caller to indicate expected use.
+func (m *MockService) EXPECT() *MockServiceMockRecorder {
+	return m.recorder
+}
+
+// AddDevice mocks base method.
+func (m *MockService) AddDevice(arg0 models.DeviceInput, arg1 string) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "AddDevice", arg0, arg1)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// AddDevice indicates an expected call of AddDevice.
+func (mr *MockServiceMockRecorder) AddDevice(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "AddDevice", reflect.TypeOf((*MockService)(nil).AddDevice), arg0, arg1)
+}
+
+// Control mocks base method.
+func (m *MockService) Control(arg0, arg1, arg2 string) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "Control", arg0, arg1, arg2)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// Control indicates an expected call of Control.
+func (mr *MockServiceMockRecorder) Control(arg0, arg1, arg2 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Control", reflect.TypeOf((*MockService)(nil).Control), arg0, arg1, arg2)
+}
+
+// DeleteDevice mocks base method.
+func (m *MockService) DeleteDevice(arg0 string) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "DeleteDevice", arg0)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// DeleteDevice indicates an expected call of DeleteDevice.
+func (mr *MockServiceMockRecorder) DeleteDevice(arg0 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "DeleteDevice", reflect.TypeOf((*MockService)(nil).DeleteDevice), arg0)
+}
+
+// GetAllDevices mocks base method.
+func (m *MockService) GetAllDevices(arg0 string) ([]models.Device, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetAllDevices", arg0)
+	ret0, _ := ret[0].([]models.Device)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// GetAllDevices indicates an expected call of GetAllDevices.
+func (mr *MockServiceMockRecorder) GetAllDevices(arg0 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetAllDevices", reflect.TypeOf((*MockService)(nil).GetAllDevices), arg0)
+}
+
+// GetDatafromWebhook mocks base method.
+func (m *MockService) GetDatafromWebhook(arg0, arg1 string) (models.ConnectionDat, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetDatafromWebhook", arg0, arg1)
+	ret0, _ := ret[0].(models.ConnectionDat)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// GetDatafromWebhook indicates an expected call of GetDatafromWebhook.
+func (mr *MockServiceMockRecorder) GetDatafromWebhook(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetDatafromWebhook", reflect.TypeOf((*MockService)(nil).GetDatafromWebhook), arg0, arg1)
+}
+
+// GetDeviceBrands mocks base method.
+func (m *MockService) GetDeviceBrands() ([]models.Device, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetDeviceBrands")
+	ret0, _ := ret[0].([]models.Device)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// GetDeviceBrands indicates an expected call of GetDeviceBrands.
+func (mr *MockServiceMockRecorder) GetDeviceBrands() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetDeviceBrands", reflect.TypeOf((*MockService)(nil).GetDeviceBrands))
+}
+
+// GetDeviceById mocks base method.
+func (m *MockService) GetDeviceById(arg0, arg1 string) (models.Device, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetDeviceById", arg0, arg1)
+	ret0, _ := ret[0].(models.Device)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// GetDeviceById indicates an expected call of GetDeviceById.
+func (mr *MockServiceMockRecorder) GetDeviceById(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetDeviceById", reflect.TypeOf((*MockService)(nil).GetDeviceById), arg0, arg1)
+}
+
+// GetDeviceHistory mocks base method.
+func (m *MockService) GetDeviceHistory(arg0 string) ([]models.DeviceHistory, string, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetDeviceHistory", arg0)
+	ret0, _ := ret[0].([]models.DeviceHistory)
+	ret1, _ := ret[1].(string)
+	ret2, _ := ret[2].(error)
+	return ret0, ret1, ret2
+}
+
+// GetDeviceHistory indicates an expected call of GetDeviceHistory.
+func (mr *MockServiceMockRecorder) GetDeviceHistory(arg0 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetDeviceHistory", reflect.TypeOf((*MockService)(nil).GetDeviceHistory), arg0)
+}
+
+// GetDeviceHistoryById mocks base method.
+func (m *MockService) GetDeviceHistoryById(arg0, arg1 string) ([]models.DeviceHistory, string, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetDeviceHistoryById", arg0, arg1)
+	ret0, _ := ret[0].([]models.DeviceHistory)
+	ret1, _ := ret[1].(string)
+	ret2, _ := ret[2].(error)
+	return ret0, ret1, ret2
+}
+
+// GetDeviceHistoryById indicates an expected call of GetDeviceHistoryById.
+func (mr *MockServiceMockRecorder) GetDeviceHistoryById(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetDeviceHistoryById", reflect.TypeOf((*MockService)(nil).GetDeviceHistoryById), arg0, arg1)
+}
+
+// PostControlAntares mocks base method.
+func (m *MockService) PostControlAntares(arg0, arg1, arg2, arg3 string) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "PostControlAntares", arg0, arg1, arg2, arg3)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// PostControlAntares indicates an expected call of PostControlAntares.
+func (mr *MockServiceMockRecorder) PostControlAntares(arg0, arg1, arg2, arg3 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "PostControlAntares", reflect.TypeOf((*MockService)(nil).PostControlAntares), arg0, arg1, arg2, arg3)
+}
+
+// UpdateDeviceById mocks base method.
+func (m *MockService) UpdateDeviceById(arg0 models.DeviceInput, arg1 string) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "UpdateDeviceById", arg0, arg1)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// UpdateDeviceById indicates an expected call of UpdateDeviceById.
+func (mr *MockServiceMockRecorder) UpdateDeviceById(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "UpdateDeviceById", reflect.TypeOf((*MockService)(nil).UpdateDeviceById), arg0, arg1)
+}
--- modules/v1/utilities/device/service/service_test.go
+++ modules/v1/utilities/device/service/service_test.go
@@ -1,80 +1,79 @@
 package service
 
-// import (
-// 	"os"
-// 	"testing"
-// 	"time"
+import (
+	"os"
+	"testing"
 
-// 	"GuppyTech/modules/v1/utilities/device/models"
-// 	m_deviceRepository "GuppyTech/modules/v1/utilities/device/repository/mock"
+	"GuppyTech/modules/v1/utilities/device/models"
+	m_deviceRepository "GuppyTech/modules/v1/utilities/device/repository/mock"
 
-// 	"github.com/gin-gonic/gin"
-// 	"github.com/golang/mock/gomock"
-// 	"github.com/stretchr/testify/assert"
-// )
+	"github.com/gin-gonic/gin"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
 
-// func TestInit(t *testing.T) {
-// 	gin.SetMode(gin.TestMode)
-// 	os.Chdir("../../../../../")
-// }
+func TestInit(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	os.Chdir("../../../../../")
+}
 
-// func Test_GetDatafromWebhook(t *testing.T) {
-// 	ctrl := gomock.NewController(t)
-// 	defer ctrl.Finish()
+func Test_GetDatafromWebhook(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 
-// 	data := models.ConnectionDat{
-// 		Status_device:    10,
-// 		Device_mode:      2,
-// 		Temperature:      0,
-// 		Ph:               0,
-// 		Dissolved_oxygen: 0,
-// 	}
-// 	antares_id := "ps9t5UiX15TVLxYB"
-// 	inputJSON := `{"aeratorMode": 2, "statusDevice": 0}`
-// 	output := models.Device{
-// 		Device_id:  "e5d415f7-a96b-4dc2-84b8-64a1830b4c01",
-// 		Antares_id: "ps9t5UiX15TVLxYB",
-// 		Status_id:  "1",
-// 	}
+	data := models.ConnectionDat{
+		Status_device:    10,
+		Device_mode:      2,
+		Temperature:      0,
+		Ph:               0,
+		Dissolved_oxygen: 0,
+	}
+	antares_id := "ps9t5UiX15TVLxYB"
+	inputJSON := `{"aeratorMode": 2, "statusDevice": 0}`
+	output := models.Device{
+		Device_id:  "e5d415f7-a96b-4dc2-84b8-64a1830b4c01",
+		Antares_id: "ps9t5UiX15TVLxYB",
+		Status_id:  "1",
+	}
 
-// 	tests := []struct {
-// 		nameTest   string
-// 		beforeTest func(service *m_deviceRepository.MockRepository)
-// 		err        error
-// 	}{
-// 		{
-// 			nameTest: "Success Post to Antares",
-// 			beforeTest: func(deviceService *m_deviceRepository.MockRepository) {
-// 				deviceService.EXPECT().GetDeviceByAntares(antares_id).Return(output, nil)
-// 				deviceService.EXPECT().BindSensorData(output.Device_id, data).Return(nil, nil).AnyTimes()
-// 			},
-// 			err: nil,
-// 		},
-// 	}
+	tests := []struct {
+		nameTest   string
+		beforeTest func(service *m_deviceRepository.MockRepository)
+		err        error
+	}{
+		{
+			nameTest: "Success Post to Antares",
+			beforeTest: func(deviceService *m_deviceRepository.MockRepository) {
+				deviceService.EXPECT().GetDeviceByAntares(antares_id).Return(output, nil)
+				deviceService.EXPECT().BindSensorData(output.Device_id, data).Return(nil, nil).AnyTimes()
+			},
+			err: nil,
+		},
+	}
 
-// 	for _, test := range tests {
-// 		t.Run(test.nameTest, func(t *testing.T) {
-// 			mockRepository := m_deviceRepository.NewMockRepository(ctrl)
+	for _, test := range tests {
+		t.Run(test.nameTest, func(t *testing.T) {
+			mockRepository := m_deviceRepository.NewMockRepository(ctrl)
 
-// 			w := &service{
-// 				repository: mockRepository,
-// 			}
+			w := &service{
+				repository: mockRepository,
+			}
 
-// 			if test.beforeTest != nil {
-// 				test.beforeTest(mockRepository)
-// 			}
+			if test.beforeTest != nil {
+				test.beforeTest(mockRepository)
+			}
 
-// 			_, err := w.GetDatafromWebhook(inputJSON, antares_id)
-// 			if err != nil {
-// 				assert.Error(t, err)
-// 			} else {
-// 				assert.NoError(t, err)
-// 			}
+			_, err := w.GetDatafromWebhook(inputJSON, antares_id)
+			if err != nil {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
 
-// 			assert.Equal(t, test.err, err)
-// 		})
-// 	}
-// }
+			assert.Equal(t, test.err, err)
+		})
+	}
+}
 
 // func Test_GetAllDevices(t *testing.T) {
 // 	ctrl := gomock.NewController(t)
@@ -181,98 +180,98 @@ package service
 // 	}
 // }
 
-// func Test_Control(t *testing.T) {
-// 	ctrl := gomock.NewController(t)
-// 	defer ctrl.Finish()
+func Test_Control(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 
-// 	id := "e5d415f7-a96b-4dc2-84b8-64a1830b4c01"
-// 	power := "11"
-// 	mode := "2"
+	id := "e5d415f7-a96b-4dc2-84b8-64a1830b4c01"
+	power := "11"
+	mode := "2"
 
-// 	tests := []struct {
-// 		nameTest   string
-// 		beforeTest func(service *m_deviceRepository.MockRepository)
-// 		result     error
-// 	}{
-// 		{
-// 			nameTest: "Success Controlling Device",
-// 			beforeTest: func(deviceService *m_deviceRepository.MockRepository) {
-// 				deviceService.EXPECT().Control(id, power, mode).Return(nil)
-// 			},
-// 			result: nil,
-// 		},
-// 	}
+	tests := []struct {
+		nameTest   string
+		beforeTest func(service *m_deviceRepository.MockRepository)
+		result     error
+	}{
+		{
+			nameTest: "Success Controlling Device",
+			beforeTest: func(deviceService *m_deviceRepository.MockRepository) {
+				deviceService.EXPECT().Control(id, power, mode).Return(nil)
+			},
+			result: nil,
+		},
+	}
 
-// 	for _, test := range tests {
-// 		t.Run(test.nameTest, func(t *testing.T) {
-// 			mockRepository := m_deviceRepository.NewMockRepository(ctrl)
+	for _, test := range tests {
+		t.Run(test.nameTest, func(t *testing.T) {
+			mockRepository := m_deviceRepository.NewMockRepository(ctrl)
 
-// 			w := &service{
-// 				repository: mockRepository,
-// 			}
+			w := &service{
+				repository: mockRepository,
+			}
 
-// 			if test.beforeTest != nil {
-// 				test.beforeTest(mockRepository)
-// 			}
+			if test.beforeTest != nil {
+				test.beforeTest(mockRepository)
+			}
 
-// 			err := w.Control(id, power, mode)
-// 			if err != nil {
-// 				assert.Error(t, err)
-// 			} else {
-// 				assert.NoError(t, err)
-// 			}
-// 		})
-// 	}
-// }
+			err := w.Control(id, power, mode)
+			if err != nil {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
 
-// func Test_PostControlAntares(t *testing.T) {
-// 	ctrl := gomock.NewController(t)
-// 	defer ctrl.Finish()
+func Test_PostControlAntares(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 
-// 	antares_id := "ps9t5UiX15TVLxYB"
-// 	token := "862b34fe2de548cc:cdf66d91b12db8d2"
-// 	power := "0"
-// 	mode := "2"
+	antares_id := "ps9t5UiX15TVLxYB"
+	token := "862b34fe2de548cc:cdf66d91b12db8d2"
+	power := "0"
+	mode := "2"
 
-// 	tests := []struct {
-// 		nameTest   string
-// 		beforeTest func(service *m_deviceRepository.MockRepository)
-// 		result     error
-// 	}{
-// 		{
-// 			nameTest: "Success Post to Antares",
-// 			beforeTest: func(deviceService *m_deviceRepository.MockRepository) {
-// 				deviceService.EXPECT().PostControlAntares(antares_id, token, power, mode).Return(nil)
-// 			},
-// 			result: nil,
-// 		},
-// 		{
-// 			nameTest: "Success Post to Antares",
-// 			beforeTest: func(deviceService *m_deviceRepository.MockRepository) {
-// 				deviceService.EXPECT().PostControlAntares(antares_id, token, power, mode).Return(nil)
-// 			},
-// 			result: nil,
-// 		},
-// 	}
+	tests := []struct {
+		nameTest   string
+		beforeTest func(service *m_deviceRepository.MockRepository)
+		result     error
+	}{
+		{
+			nameTest: "Success Post to Antares",
+			beforeTest: func(deviceService *m_deviceRepository.MockRepository) {
+				deviceService.EXPECT().PostControlAntares(antares_id, token, power, mode).Return(nil)
+			},
+			result: nil,
+		},
+		{
+			nameTest: "Success Post to Antares",
+			beforeTest: func(deviceService *m_deviceRepository.MockRepository) {
+				deviceService.EXPECT().PostControlAntares(antares_id, token, power, mode).Return(nil)
+			},
+			result: nil,
+		},
+	}
 
-// 	for _, test := range tests {
-// 		t.Run(test.nameTest, func(t *testing.T) {
-// 			mockRepository := m_deviceRepository.NewMockRepository(ctrl)
+	for _, test := range tests {
+		t.Run(test.nameTest, func(t *testing.T) {
+			mockRepository := m_deviceRepository.NewMockRepository(ctrl)
 
-// 			w := &service{
-// 				repository: mockRepository,
-// 			}
+			w := &service{
+				repository: mockRepository,
+			}
 
-// 			if test.beforeTest != nil {
-// 				test.beforeTest(mockRepository)
-// 			}
+			if test.beforeTest != nil {
+				test.beforeTest(mockRepository)
+			}
 
-// 			err := w.PostControlAntares(antares_id, token, power, mode)
-// 			if err != nil {
-// 				assert.Error(t, err)
-// 			} else {
-// 				assert.NoError(t, err)
-// 			}
-// 		})
-// 	}
-// }
+			err := w.PostControlAntares(antares_id, token, power, mode)
+			if err != nil {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}

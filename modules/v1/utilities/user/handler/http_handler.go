dc8e2c882bd8637689d0ce6483bd1b32ddffe431
Habibmahaasin <habibirf23@gmail.com>
2023-06-26 02:53:43 +0700

    set timeout to 6 hours

--- modules/v1/utilities/user/handler/http_handler.go
+++ modules/v1/utilities/user/handler/http_handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"GuppyTech/app/config"
 	"GuppyTech/modules/v1/utilities/user/models"
 	"log"
 	"net/http"
@@ -10,6 +11,7 @@ import (
 )
 
 func (n *userHandler) Login(c *gin.Context) {
+	conf, _ := config.Init()
 	session := sessions.Default(c)
 	var input models.LoginInput
 
@@ -30,13 +32,17 @@ func (n *userHandler) Login(c *gin.Context) {
 	}
 
 	token, _ := n.jwtoken.GenerateToken(user.User_id, user.Full_name, user.Role_id)
-	c.SetCookie("Token", token, 90, "/", "guppy.tech", false, true)
+	if conf.App.Mode == "debug" {
+		c.SetCookie("Token", token, 21600, "/", "localhost", false, true)
+	} else {
+		c.SetCookie("Token", token, 21600, "/", "guppy.tech", false, true)
+	}
 
 	session.Set("email", user.Email)
 	session.Set("full_name", user.Full_name)
 	session.Set("user_id", user.User_id)
 	session.Options(sessions.Options{
-		MaxAge: 90,
+		MaxAge: 21600,
 	})
 	session.Save()
 

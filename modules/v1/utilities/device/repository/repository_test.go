b2af3d001a8c1c643969fd4706cd7a876df51d2b
Habibmahaasin <habibirf23@gmail.com>
2023-06-29 23:56:59 +0700

    update unit testing repository coverage 100%

--- modules/v1/utilities/device/repository/repository_test.go
+++ modules/v1/utilities/device/repository/repository_test.go
@@ -220,3 +220,142 @@ func (s *Suite) Test_repository_PostControlAntares() {
 	err = repo.PostControlAntares("ps9t5UiX15TVLxYB", "862b34fe2de548cc:cdf66d91b12db8d2", "1", "2")
 	require.Error(s.T(), err)
 }
+
+func (s *Suite) Test_repository_AddDevice() {
+	user_id := "a962321c-6b3a-4b92-8a70-9729a1f15b75"
+	deviceInput := models.DeviceInput{
+		Device_name:     "Aerator Utama",
+		Antares_id:      "ps9t5UiX15TVLxYB",
+		Device_location: "Jl. Raya Bogor",
+		Latitude:        "1.1",
+		Longitude:       "1.1",
+		Brand_id:        "1",
+		Mode_id:         "2",
+	}
+
+	s.mock.ExpectExec(regexp.QuoteMeta("INSERT INTO devices (device_id, device_name, antares_id, device_location, status_id, latitude, longitude, brand_id, user_id, mode_id, date_created, date_updated) VALUES ($1,$2,$3,$4,10,$5,$6,$7,$8,$9,now(),now())")).
+		WithArgs(
+			sqlmock.AnyArg(),
+			deviceInput.Device_name,
+			deviceInput.Antares_id,
+			deviceInput.Device_location,
+			deviceInput.Latitude,
+			deviceInput.Longitude,
+			deviceInput.Brand_id,
+			user_id,
+			deviceInput.Mode_id,
+		).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	err := s.repository.AddDevice(deviceInput, user_id)
+	require.NoError(s.T(), err)
+}
+
+func (s *Suite) Test_repository_GetDeviceById() {
+	user_id := "a962321c-6b3a-4b92-8a70-9729a1f15b75"
+	device_id := "e5d415f7-a96b-4dc2-84b8-64a1830b4c01"
+	expected_result := models.Device{
+		Device_id:       "e5d415f7-a96b-4dc2-84b8-64a1830b4c01",
+		Device_name:     "Aerator Utama",
+		Antares_id:      "ps9t5UiX15TVLxYB",
+		Device_location: "Aquarium Kiri Hitam",
+		Status_name:     "Tidak Aktif",
+		Mode_name:       "Otomatis",
+		Brand_name:      "Amara-666",
+	}
+	rows := sqlmock.NewRows([]string{"device_id", "device_name", "antares_id", "device_location", "status_name", "mode_name", "brand_name"}).AddRow(expected_result.Device_id, expected_result.Device_name, expected_result.Antares_id, expected_result.Device_location, expected_result.Status_name, expected_result.Mode_name, expected_result.Brand_name)
+
+	s.mock.ExpectQuery(regexp.QuoteMeta("select d.device_id, d.antares_id, d.device_name, d.device_location, d.mode_id, dm.mode_name, d.status_id, ds.status_name, d.brand_id,b.brand_name, d.user_id, d.latitude, d.longitude, d.date_created, d.date_updated from devices d inner join device_status ds ON d.status_id = ds.status_id inner join device_mode dm on d.mode_id = dm.mode_id inner join brand b on b.brand_id = d.brand_id where d.device_id = $1 and user_id = $2")).
+		WithArgs(device_id, user_id).
+		WillReturnRows(rows)
+
+	resp, err := s.repository.GetDeviceById(user_id, device_id)
+	require.NoError(s.T(), err)
+	require.Equal(s.T(), expected_result, resp)
+}
+
+func (s *Suite) Test_repository_GetDeviceHistoryById() {
+	user_id := "a962321c-6b3a-4b92-8a70-9729a1f15b75"
+	device_id := "e5d415f7-a96b-4dc2-84b8-64a1830b4c01"
+
+	expected_result := []models.DeviceHistory{
+		{
+			History_device_id:   "e5d415f7-a96b-4dc2-84b8-64a1830b4c01",
+			History_device_name: "Aerator Utama",
+			History_status_name: "Tidak Aktif",
+			History_mode_name:   "Otomatis",
+		},
+	}
+
+	rows := sqlmock.NewRows([]string{"device_id", "device_name", "status_name", "mode_name"}).AddRow(expected_result[0].History_device_id, expected_result[0].History_device_name, expected_result[0].History_status_name, expected_result[0].History_mode_name)
+
+	s.mock.ExpectQuery(regexp.QuoteMeta("select d.device_id, d.device_name, ds.status_name, dm.mode_name, dh.ph, dh.temperature, dh.dissolved_oxygen, dh.history_date from  device_history dh inner join devices d on dh.device_id = d.device_id inner join device_status ds on dh.status_id = ds.status_id inner join device_mode dm on dh.mode_id = dm.mode_id where d.device_id = $1 and d.user_id = $2 ORDER BY dh.history_id DESC")).
+		WithArgs(device_id, user_id).
+		WillReturnRows(rows)
+
+	resp, err := s.repository.GetDeviceHistoryById(device_id, user_id)
+	require.NoError(s.T(), err)
+	require.Equal(s.T(), expected_result, resp)
+}
+
+func (s *Suite) Test_repository_GetDeviceBrands() {
+	expected_result := []models.Device{
+		{
+			Brand_id:   1,
+			Brand_name: "Amara AA-666",
+		},
+	}
+
+	rows := sqlmock.NewRows([]string{"brand_id", "brand_name"}).AddRow(expected_result[0].Brand_id, expected_result[0].Brand_name)
+
+	s.mock.ExpectQuery(regexp.QuoteMeta("select * from brand")).
+		WillReturnRows(rows)
+
+	resp, err := s.repository.GetDeviceBrands()
+	require.NoError(s.T(), err)
+	require.Equal(s.T(), expected_result, resp)
+}
+
+func (s *Suite) Test_repository_DeleteDevice() {
+	d_id := "e5d415f7-a96b-4dc2-84b8-64a1830b4c01"
+
+	s.mock.ExpectExec(regexp.QuoteMeta("DELETE FROM device_history WHERE device_id = $1")).
+		WithArgs(d_id).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	s.mock.ExpectExec(regexp.QuoteMeta("DELETE FROM devices WHERE device_id = $1")).
+		WithArgs(d_id).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	err := s.repository.DeleteDevice(d_id)
+	require.NoError(s.T(), err)
+}
+
+func (s *Suite) Test_repository_UpdateDeviceById() {
+	device_id := "e5d415f7-a96b-4dc2-84b8-64a1830b4c01"
+	deviceInput := models.DeviceInput{
+		Device_name:     "Aerator Utama",
+		Antares_id:      "ps9t5UiX15TVLxYB",
+		Device_location: "Jl. Raya Bogor",
+		Latitude:        "1.1",
+		Longitude:       "1.1",
+		Brand_id:        "1",
+		Mode_id:         "2",
+	}
+
+	s.mock.ExpectExec(regexp.QuoteMeta(`update devices set device_name = $1, antares_id = $2, device_location = $3, latitude = $4, longitude = $5, brand_id = $6, mode_id = $7, date_updated = now() where device_id = $8`)).
+		WithArgs(
+			deviceInput.Device_name,
+			deviceInput.Antares_id,
+			deviceInput.Device_location,
+			deviceInput.Latitude,
+			deviceInput.Longitude,
+			deviceInput.Brand_id,
+			deviceInput.Mode_id,
+			device_id,
+		).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	err := s.repository.UpdateDeviceById(deviceInput, device_id)
+	require.NoError(s.T(), err)
+}

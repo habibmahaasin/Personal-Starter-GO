5f422307603346d86953d8ee791005d6a56dbd39
Habibmahaasin <habibirf23@gmail.com>
2023-07-11 00:01:30 +0700

    update sql file icon logo query history

--- modules/v1/utilities/device/repository/repository.go
+++ modules/v1/utilities/device/repository/repository.go
@@ -34,7 +34,7 @@ func (r *repository) GetDeviceByAntares(antaresDeviceID string) (models.Device, 
 
 func (r *repository) GetDeviceHistory(user_id string) ([]models.DeviceHistory, error) {
 	var DeviceHistory []models.DeviceHistory
-	err := r.db.Raw("select d.device_id, d.device_name, ds.status_name, dm.mode_name, dh.ph, dh.temperature, dh.dissolved_oxygen, dh.history_date from  device_history dh inner join devices d on dh.device_id = d.device_id inner join device_status ds on dh.status_id = ds.status_id inner join device_mode dm on dh.mode_id = dm.mode_id where d.user_id = ? ORDER BY dh.history_id DESC LIMIT 250", user_id).Scan(&DeviceHistory).Error
+	err := r.db.Raw("select d.device_id, d.device_name, ds.status_name, dm.mode_name, dh.ph, dh.temperature, dh.dissolved_oxygen, dh.history_date from  device_history dh inner join devices d on dh.device_id = d.device_id inner join device_status ds on dh.status_id = ds.status_id inner join device_mode dm on dh.mode_id = dm.mode_id where d.user_id = ? and NOT(dh.dissolved_oxygen = 0) ORDER BY dh.history_id DESC LIMIT 250", user_id).Scan(&DeviceHistory).Error
 	return DeviceHistory, err
 }
 
@@ -87,7 +87,7 @@ func (r *repository) GetDeviceById(u_id string, d_id string) (models.Device, err
 
 func (r *repository) GetDeviceHistoryById(d_id string, u_id string) ([]models.DeviceHistory, error) {
 	var DeviceHistory []models.DeviceHistory
-	err := r.db.Raw("select d.device_id, d.device_name, ds.status_name, dm.mode_name, dh.ph, dh.temperature, dh.dissolved_oxygen, dh.history_date from  device_history dh inner join devices d on dh.device_id = d.device_id inner join device_status ds on dh.status_id = ds.status_id inner join device_mode dm on dh.mode_id = dm.mode_id where d.device_id = ? and d.user_id = ? ORDER BY dh.history_id DESC", d_id, u_id).Scan(&DeviceHistory).Error
+	err := r.db.Raw("select d.device_id, d.device_name, ds.status_name, dm.mode_name, dh.ph, dh.temperature, dh.dissolved_oxygen, dh.history_date from  device_history dh inner join devices d on dh.device_id = d.device_id inner join device_status ds on dh.status_id = ds.status_id inner join device_mode dm on dh.mode_id = dm.mode_id where d.device_id = ? and d.user_id = ? and NOT(dh.dissolved_oxygen = 0) ORDER BY dh.history_id DESC", d_id, u_id).Scan(&DeviceHistory).Error
 	return DeviceHistory, err
 }
 
